1a5c5153e6cb3ceb9cdc3e8bc3ef745839538c65
Nathan_Pflier <n.pflier@student.isartdigital.com>
2020-06-23 17:16:30 +0100

    Start faetureManager

---
+++ MoveExperience/Assets/_MoveExperience/Scripts/Feature/AFeature.cs
@@ -0,0 +1,23 @@
+///-----------------------------------------------------------------
+/// Author : Nathan_Pflier
+/// Date : 23/06/2020 17:39
+///-----------------------------------------------------------------
+
+using UnityEngine;
+
+namespace Com.HolidayGame.MoveExperience.Feature {
+	public class AFeature : MonoBehaviour {
+	
+		private void Start () {
+			
+		}
+		
+		private void Update () {
+			
+		}
+
+		virtual public void On() { }
+
+		virtual public void Off() { }
+	}
+}
---
+++ MoveExperience/Assets/_MoveExperience/Scripts/Feature/DefaultFeature.cs
@@ -0,0 +1,19 @@
+///-----------------------------------------------------------------
+/// Author : Nathan_Pflier
+/// Date : 23/06/2020 17:57
+///-----------------------------------------------------------------
+
+using UnityEngine;
+
+namespace Com.HolidayGame.MoveExperience.Feature {
+	public class DefaultFeature : AFeature {
+	
+		private void Start () {
+			
+		}
+		
+		private void Update () {
+			
+		}
+	}
+}
