29f4b6db7112d6942146e2a18fa130f01e572566
Nathan_Pflier <n.pflier@student.isartdigital.com>
2020-06-23 11:33:55 +0100

    controller class

---
+++ MoveExperience/Assets/_MoveExperience/Scripts/Controller.cs
@@ -0,0 +1,19 @@
+///-----------------------------------------------------------------
+/// Author : Nathan_Pflier
+/// Date : 23/06/2020 12:18
+///-----------------------------------------------------------------
+
+using UnityEngine;
+
+namespace Com.HolidayGame.MoveExperience {
+	public class Controller : MonoBehaviour {
+	
+		private void Start () {
+			
+		}
+		
+		private void Update () {
+			
+		}
+	}
+}
