3bbfb647a95d7aed9489149c74605e5841c02a0e
Nathan_Pflier <n.pflier@student.isartdigital.com>
2020-06-23 12:04:53 +0100

    Controller V0.1

--- MoveExperience/Assets/_MoveExperience/Scripts/Controller.cs
+++ MoveExperience/Assets/_MoveExperience/Scripts/Controller.cs
@@ -7,13 +7,33 @@ using UnityEngine;
 
 namespace Com.HolidayGame.MoveExperience {
 	public class Controller : MonoBehaviour {
-	
+
+		[SerializeField] private string horizontalAxis;
+		[SerializeField] private string verticalAxis;
+		[SerializeField] private string jumpAxis;
+
+		private float _getAxisHorizontal;
+		private float _getAxisVertical;
+		private bool _jump;
+
+		public float GetAxisHorizontal {
+			get { return _getAxisHorizontal; }
+		}
+		public float GetAxisVertical {
+			get { return _getAxisVertical; }
+		}
+		public bool Jump {
+			get { return _jump; }
+		}
+
 		private void Start () {
 			
 		}
 		
 		private void Update () {
-			
+			_getAxisHorizontal = Input.GetAxis (horizontalAxis);
+			_getAxisVertical = Input.GetAxis (verticalAxis);
+			_jump = Input.GetAxis (jumpAxis) == 1;
 		}
 	}
 }
