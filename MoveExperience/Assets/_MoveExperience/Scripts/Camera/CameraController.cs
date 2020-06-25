31a91649ea2da24a8836aa10553cd4d9b1ca2530
ClementVieilly <c.vieilly@student.isartdigital.com>
2020-06-25 12:42:36 +0200

    Camera on Circle Must be replaced if player move

--- MoveExperience/Assets/_MoveExperience/Scripts/Camera/CameraController.cs
+++ MoveExperience/Assets/_MoveExperience/Scripts/Camera/CameraController.cs
@@ -11,11 +11,24 @@ namespace Com.HolidayGame.MoveExperience.Camera {
 
 		[SerializeField] private Transform player = default;
 		[SerializeField] private float offSetY = 10f;
-		[SerializeField] private float offSetZ = 8f; 
+		[SerializeField] private float offSetZ = 8f;
+		[SerializeField, Range(0, 20)] private float radius;
+		private float horizontalAngle = 1;
+		private float verticalAngle = 1;
+		private Vector3 newDirection;
 		private void Update()
 		{
+			horizontalAngle += player.rotation.y * Time.deltaTime * 1;
+		
+			//transform.position = new Vector3(player.position.x, player.position.y + offSetY, player.position.z - offSetZ);
+
+			newDirection.x = radius * Mathf.Cos(verticalAngle) * Mathf.Cos(horizontalAngle);
+			newDirection.y = radius * Mathf.Sin(verticalAngle);
+			newDirection.z = radius * Mathf.Cos(verticalAngle) * Mathf.Sin(horizontalAngle);
+
+
+			transform.position = newDirection;
 			transform.rotation = Quaternion.LookRotation(player.position - transform.position, Vector3.up);
-			transform.position = new Vector3(player.position.x, player.position.y + offSetY, player.position.z - offSetZ);
 		}
 
 		private void OnValidate()
